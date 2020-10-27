bd47cf53b416020959f3917e95acf878b99c2806
Lily Lin <lily.lin@linecorp.com>
2020-10-27 16:42:05 +0800

    add parse input

--- src/main/java/BudgetService.java
+++ src/main/java/BudgetService.java
@@ -1,4 +1,7 @@
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BudgetService {
     public BudgetService(IBudgetRepo repo) {
@@ -6,6 +9,31 @@ public class BudgetService {
     }
 
     public double query(LocalDate start, LocalDate end) {
+        List<DatePeriod> dps = parseInput(start, end);
+
         return 0.0;
+    }
+
+    public List<DatePeriod> parseInput(LocalDate startInput, LocalDate endInput) {
+        LocalDate start = startInput;
+        LocalDate lastDayOfMonth= start.with(TemporalAdjusters.lastDayOfMonth());
+
+        List<DatePeriod> periods = new ArrayList<DatePeriod>();
+        while (lastDayOfMonth.isBefore(endInput)) {
+            DatePeriod dp = new DatePeriod();
+            dp.start = start;
+            dp.end = lastDayOfMonth;
+            periods.add(dp);
+
+            start = lastDayOfMonth.with(TemporalAdjusters.firstDayOfNextMonth());
+            lastDayOfMonth = start.with(TemporalAdjusters.lastDayOfMonth());
+        }
+
+        DatePeriod dp = new DatePeriod();
+        dp.start = start;
+        dp.end = endInput;
+        periods.add(dp);
+
+        return periods;
     }
 }
---
+++ src/main/java/DatePeriod.java
@@ -0,0 +1,6 @@
+import java.time.LocalDate;
+
+public class DatePeriod {
+    LocalDate start;
+    LocalDate end;
+}
